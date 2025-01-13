3c89778e7ca09fe96787dbef64d029e4026bdaa0
Rajan-007 <aravinthrajan034@gmail.com>
2025-01-13 23:46:01 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -198,6 +198,11 @@ app.get('/users', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.json({ message: 'API is running successfully 🚀' });
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
